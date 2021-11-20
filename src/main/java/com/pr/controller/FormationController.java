79bd0c2d63a46382b8e73bde1948d3f8ac23205f
khalil <khalillakhdharatc@gmail.com>
2021-11-20 14:50:15 +0100

    Update FormationController.java

--- src/main/java/com/pr/controller/FormationController.java
+++ src/main/java/com/pr/controller/FormationController.java
@@ -1,11 +1,14 @@
 package com.pr.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +29,18 @@ public Formation addFormation(@Valid @RequestBody Formation f)
 {
 return formationService.addFormation(f);	
 }
+@GetMapping("formation/{id}")
+public Optional<Formation> getFormationsById(@PathVariable long id)
+{
+	return formationService.getFormationById(id);
+}
 
 
+@DeleteMapping("/formation/{id}")
+public void deleteStudent(@PathVariable long id) {
 
+	formationService.deleteFormation(id);
+}
 
 
 }
